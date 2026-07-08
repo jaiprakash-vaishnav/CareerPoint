2d0c380bf1a5b32cb15b23d12fb7410019289edf
jaiprakash <jaiprakashv920@gmail.com>
2026-07-08 21:18:13 +0530

    login page setup

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import User from "@/layout/client/index.jsx";
-function LoginComponent(){
-    return (
-        <User>
-            <div>
-                Login Component
-            </div>
-        </User>
-    )
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import styles from "./style.module.css";
+function LoginComponent() {
+  const authState = useSelector((state) => state.auth);
+  const router = useRouter();
+  const [userLoginMethod, setUserLoginMethod] = useState(false);
+  useEffect(() => {
+    if (authState.logedIn) {
+      router.push("/dashboard");
+    }
+  });
+  return (
+    <User>
+      <div className={styles.container}>
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}>
+            <p className={styles.cardLeftHeading}> {userLoginMethod ? "Sign In" : "Sign Up"}</p>
+          </div>
+          <div className={styles.cardContainerRight}></div>
+        </div>
+      </div>
+    </User>
+  );
 }
 
 export default LoginComponent;
